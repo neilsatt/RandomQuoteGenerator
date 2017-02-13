abb080672265f40b1e46472a433ffdad544977a6
Neil Satterfield <neilsatt@gmail.com>
2017-02-12 18:09:20 -0700

    project updates

---
+++ js/quotes.js
@@ -0,0 +1,27 @@
+var quotes = [ 
+  { 
+    quote: 'I find that the harder I work, the more luck I seem to have.',
+    source: 'Thomas Jefferson',
+    years: '(1743-1826)'
+  },
+  {
+    quote: 'When you gaze long into the abyss, the abyss also gazes into you.',
+    source: 'Friedrich Nietzsche',
+    years: '(1844-1900)'
+  },
+  {
+    quote: 'I have never let my schooling interfere with my education.',
+    source: 'Mark Twain',
+    years: '(1835-1910)'
+  },
+  {
+    quote: "I'm living so far beyond my income that we may almost be said to be living apart.",
+    source: 'e. e. cummings',
+    years: '(1894-1962)'
+  },
+  {
+    quote: "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+    source: 'Maya Angelou',
+    years: "(1928-)"
+  }
+];
--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// select a random quote object from the quotes array and return the randomly selected quote object
+function getRandomQuote(){
+    randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    console.log(randomQuote)
+    return randomQuote;
+};
+
+
+
+
+
+function printQuote(){
+    //console.log('My random quote: '+randomQuote.quote);
+        var randomQuote = getRandomQuote(); // call function and store the returned quote object in a variable
+        var totalHTML = '';
+        totalHTML += "<p class='quote' >"+ randomQuote.quote + "</p>";
+        totalHTML += "<p class='source'> "+ randomQuote.source;
+        totalHTML += "<span class='years'> "+ randomQuote.years + "</p>";
+        document.getElementById('quote-box').innerHTML = totalHTML;
+}
+
+
+
